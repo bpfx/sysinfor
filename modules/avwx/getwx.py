a9c7ce679010f06140974e3c14d3f66e658e8d7d
Bob Bomar <bob@bpfx.org>
2017-04-25 13:27:50 -0500

    Adding getwx

---
+++ modules/avwx/getwx.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+
+# https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KDEN%20KSEA%20PHNL&hoursBeforeNow=2
+
+import requests
+
+URL="https://aviationweather.gov/adds/dataserver_current/httpparam"
+
+# Build airport list
+# Build query for each type
+# Make query
+
+def build_airport_list(airportList)
+  # Add %20's and such as necessary
+  return airportList.join("%20")
+
+def getmetar(airportList)
+  # In: list of airports
+  # Out: XML of data
+  
+
+def build_query(airportList,requestType)
+
+def make_query(queryToMake)
+
+airportList = [KDTO,KAFW]
+requestType = metar
